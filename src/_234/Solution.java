a08604e4a1c3a1d57851877a9d3008d9d019b00a
reiser <leonzone58@gmail.com>
2019-01-23 22:29:11 +0800

    finsi solution 234

--- src/_234/Solution.java
+++ src/_234/Solution.java
@@ -1,5 +1,7 @@
 package _234;
 
+import util.ListNode;
+
 /**
  * Author: reiserx
  * Date:2019/1/22
@@ -7,7 +9,41 @@ package _234;
  */
 public class Solution {
 
-//    public boolean isPalindrome(ListNode head) {
-//
-//    }
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+        ListNode pre = null;
+        ListNode current = head;
+        int n = 0;
+
+        while (current != null) {
+            ListNode temp = new ListNode(current.val);
+            temp.next = pre;
+            pre = temp;
+            current = current.next;
+            n++;
+        }
+
+
+        while (n > 0) {
+            if (head.val != pre.val) {
+                return false;
+            }
+            head = head.next;
+            pre = pre.next;
+            n -= 2;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.isPalindrome(ListNode.createTestData("[1,2]")));
+        System.out.println(solution.isPalindrome(ListNode.createTestData("[1,2,2,1]")));
+        System.out.println(solution.isPalindrome(ListNode.createTestData("[1,2,3,4]")));
+    }
 }
